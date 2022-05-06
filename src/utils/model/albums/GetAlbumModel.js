89e3c46a63a812def4ccb30bf2c9f8ff2cbe5840
Gregorius Ivan Sebastian <gregorius.sebastian@advotics.com>
2022-05-06 19:43:45 +0700

    Added leftover files from previous commit

---
+++ src/utils/model/albums/GetAlbumModel.js
@@ -0,0 +1,15 @@
+class GetAlbumModel {
+  constructor({ id, name, year }) {
+    this.id = id;
+    this.name = name;
+    this.year = year;
+  }
+
+  getSelectModel() {
+    return {
+      id: this.id, name: this.name, year: this.year,
+    };
+  }
+}
+
+module.exports = GetAlbumModel;
---
+++ src/utils/model/albums/PutAlbumModel.js
@@ -0,0 +1,13 @@
+class PutAlbumModel {
+  constructor({ name, year}, albumId) {
+    this.id = albumId;
+    this.name = name;
+    this.year = year;
+  }
+
+  getUpdateModel() {
+    return [this.name, this.year, this.id];
+  }
+}
+
+module.exports = PutAlbumModel;
